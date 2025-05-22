00161687870d61237cafd6c902affc28eff9077c
sok205 <Filip007lol@gmail.com>
2025-05-22 21:25:18 +0200

    Linting

--- include/server/exceptions.hpp
+++ include/server/exceptions.hpp
@@ -4,5 +4,47 @@
 
 #ifndef EXCEPTIONS_HPP
 #define EXCEPTIONS_HPP
+#include <stdexcept>
 
+
+class CustomException : public std::runtime_error
+{
+    public:
+        explicit CustomException(const std::string& msg) : std::runtime_error(msg) {}
+
+        const char* what() const noexcept override
+        {
+            return std::runtime_error::what();
+        };
+};
+
+class SocketCreationException final : CustomException
+{
+    public:
+        explicit SocketCreationException(const std::string& msg) : CustomException(msg) {}
+};
+
+class SocketOptionSet final : public std::runtime_error
+{
+    public:
+        explicit SocketOptionSet(const std::string& msg) : std::runtime_error(msg) {}
+};
+
+class BindException final : CustomException
+{
+    public:
+        explicit BindException(const std::string& msg) : CustomException(msg) {}
+};
+
+class ListenException final : CustomException
+{
+    public:
+        explicit ListenException(const std::string& msg) : CustomException(msg) {}
+};
+
+class AcceptException final : CustomException
+{
+    public:
+        explicit AcceptException(const std::string& msg) : CustomException(msg) {}
+};
 #endif //EXCEPTIONS_HPP
--- include/server/server.hpp
+++ include/server/server.hpp
@@ -4,21 +4,27 @@
 #pragma once
 
 #include <thread>
+#include <server/exceptions.hpp>
 
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+
 class TcpServer {
 public:
-    explicit TcpServer(uint16_t port) : serverFd_(::socket(AF_INET, SOCK_STREAM, 0)) {
+    explicit TcpServer(uint16_t port) : serverFd_(::socket(AF_INET, SOCK_STREAM, 0))
+    {
         // Creating a socket
 
-        if (serverFd_ < 0) throw std::runtime_error("Failed to create socket");
+        if (serverFd_ < 0)
+            throw SocketCreationException("Socket creation failed");
 
-        int reuse = 1;
+
+        constexpr int reuse = 1;
         // Setting Socket Options. This allows reusing the same address multiple times
         if (setsockopt(serverFd_, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
-            throw std::runtime_error("setsockopt failed");
+            throw SocketCreationException("Socket option set failed");
+
 
         sockaddr_in addr{};
         addr.sin_family = AF_INET; // IPv4
@@ -26,33 +32,35 @@ public:
         addr.sin_port = htons(port); // Sets the port number
 
         if (bind(serverFd_, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) != 0) // bind the socket to this address and port
-            throw std::runtime_error("Failed to bind");
+            throw BindException("Bind failed");
+
 
         if (listen(serverFd_, 5) != 0) // start listening to incoming connections
-            throw std::runtime_error("Listen failed");
+            throw ListenException("Listen failed");
     }
 
     ~TcpServer() = default;
 
-    void run() {
+    void run() const {
 
         std::vector<std::thread> serverThreads;
         while (true) {
             sockaddr_in client_addr{}; // stores information about the client
             socklen_t client_len = sizeof(client_addr); // length of the client address structure
             int client_fd = accept(serverFd_, reinterpret_cast<sockaddr*>(&client_addr), &client_len); // wait for client to connect
-            if (client_fd < 0) throw std::runtime_error("Accept failed"); // check if accept was successful
+            if (client_fd < 0)
+                throw AcceptException("Accept failed"); // check if accept was successful
 
             serverThreads.emplace_back([this, client_fd]() {
-                std::string request = read_request(client_fd); // read the request from the client
+                const std::string request = read_request(client_fd); // read the request from the client
                 std::cout << "Received request:\n" << request << std::endl; // print the request
 
                 if (check_request(request)){
-                    std::string response = "HTTP/1.1 200 OK\r\n\r\n";
+                    const std::string response = "HTTP/1.1 200 OK\r\n\r\n";
                     send(client_fd, response.c_str(), response.size(), 0);
                   }
                   else {
-                      std::string response_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
+                      const std::string response_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
                       send(client_fd, response_404.c_str(), response_404.size(), 0);
                   }
                 close(client_fd);
@@ -64,10 +72,9 @@ public:
 private:
     int serverFd_;
 
-    std::string read_request(int fd) {
+    static std::string read_request(const int fd) {
         std::vector<char> buffer(4096);
-        ssize_t bytes = recv(fd, buffer.data(), buffer.size() - 1, 0);
-        if (bytes > 0) {
+        if (const ssize_t bytes = recv(fd, buffer.data(), buffer.size() - 1, 0); bytes > 0) {
             buffer[bytes] = '\0';
             return std::string(buffer.data());
         }
@@ -76,17 +83,17 @@ private:
 
     //Check if path exists on the server
     static bool check_request(std::string const& request) {
-        std::regex path_regex(R"(^\S+\s(\S+))");
+        const std::regex path_regex(R"(^\S+\s(\S+))");
 
         if (std::smatch first_match; std::regex_search(request, first_match, path_regex))
         {
-            std::string path = first_match[1].str();
+            const std::string path = first_match[1].str();
             std::cout << "Path: "<< path << std::endl;
 
-            //Removing the leading '/' from the request
-            std::filesystem::path full_path = std::filesystem::current_path() / path.substr(1);
+            // Removing the leading '/' from the request
 
-            if (std::filesystem::exists(full_path))
+            if (const std::filesystem::path full_path = std::filesystem::current_path() / path.substr(1);
+                std::filesystem::exists(full_path))
                 return true;
         }
         return false;
